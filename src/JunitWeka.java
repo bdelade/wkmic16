6be313132c7e6755b2b44b9d6309cf177bdc6eee
bdelade <deladerriere.b@gmail.com>
2016-03-16 18:06:02 +0100

    ok

---
+++ src/JunitWeka.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
+import weka.core.FastVector;
+
+public class JunitWeka {
+
+	@Test
+	public void testwekareader() throws Exception {
+		WekaReader wr=new WekaReader();
+		assertNotNull(wr);// on test si l'objet à bien été créé
+		
+		assertNotNull(wr.getAttribute()); //on test que l'objet attribut 
+		
+		assertNotSame(wr.getTestData(),wr.getTrainData()); //on test si les instances sont différentes
+		
+		
+	}
+	@Test
+	
+	public void testwekaFilter() throws Exception {
+	
+		WekaReader wr=new WekaReader();
+		WekaFilter wf=new WekaFilter(wr);
+		assertNotNull(wf);
+		assertNotNull(wf.getAttribute()); //test attribut non nul 
+		assertNotSame(wr.getTrainData(),wf.getFTraindata()); //test si les instance filtré sont les méme que non filtré
+		assertNotSame(wr.getTrainData().numInstances(),wf.getFTraindata().numInstances()); //test du nombre d'instances
+	
+	}
+	
+	public void testwekaBuilder() throws Exception {
+		WekaReader wr=new WekaReader();
+		WekaFilter wf=new WekaFilter(wr);
+		WekaBuilder wb=new WekaBuilder();
+		assertNotNull(wb);
+		//wb.setDefaultOption();
+		assertNotSame(wb.getOneClassifier("RandomForest"),wb.getOneClassifier("J48"));
+		wb.buildOneClassifiers("RandomForest", wf.getFTraindata());
+		assertEquals(true,wb.getOneClassifier("RandomForest").getClass().isInstance(RandomForest.class.newInstance()));
+		
+
+	}
+	public void testwekaRunner() throws Exception {
+		WekaReader wr=new WekaReader();
+		WekaFilter wf=new WekaFilter(wr);
+		WekaBuilder wb=new WekaBuilder();
+		
+		wb.setDefaultOption();
+		wb.buildOneClassifiers("RandomForest", wf.getFTraindata());
+		WekaRunner wru= new WekaRunner(wf.getAttribute());	
+		assertNotNull(wru); // test si l'on a bien créé l'objet
+		FastVector trainPred = wru.predict(wb.getOneClassifier("RandomForest"),wf.getFTraindata() , wf.getFTraindata());
+		assertNotNull(trainPred); //test si la methode predic à bien fonctionné
+		double score = wru.getScore(trainPred, wf.getFTraindata());
+		assertNotNull(score); //meme chose
+		
+	}
+	
+
+}
